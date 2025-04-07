ed841bfc82d2ddef582265adf4eaa8f7fc20f2c8
ctsfy <korneliusjfp@gmail.com>
2025-04-07 14:37:30 +0700

    third commit

--- app/Livewire/Pages/CheckoutPage.php
+++ app/Livewire/Pages/CheckoutPage.php
@@ -2,6 +2,11 @@
 
 namespace App\Livewire\Pages;
 
+use App\Livewire\Traits\CartManagement;
+use App\Models\Foods;
+use Livewire\Attributes\Layout;
+use Livewire\Attributes\Validate;
+use Livewire\Attributes\Session;
 use Livewire\Component;
 
 class CheckoutPage extends Component
